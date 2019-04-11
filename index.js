77545d7b57a4d27e69257b5995b08e45f889d590
midzer <midzer@gmail.com>
2019-04-11 18:25:46 +0200

    adapt for unified build script

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-const bsn3 = require('bootstrap.native/build-module.js')
-let bsn = require('bootstrap.native/build-module-v4.js')
+const bsn = require('bootstrap.native/lib/build-module.js')
 const { getOptions } = require('loader-utils')
 
 module.exports = function () {
   this.cacheable = true
   const callback = this.async()
   const options = getOptions(this) || {}
-
-  if (options.bsVersion === 3) {
-    bsn = bsn3
-  }
 
   bsn(options).then((source) => {
     callback(null, source)
