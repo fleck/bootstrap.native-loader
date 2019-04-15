1f56edae0f554ca9dad6c89f8d5aea616d6a6c29
midzer <midzer@gmail.com>
2019-04-15 15:25:51 +0200

    default to bootstrap 4

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ module.exports = function () {
   this.cacheable = true
   const callback = this.async()
   const options = getOptions(this) || {}
+  options.bs_version = options.bs_version || 4;
 
   bsn(options).then((source) => {
     callback(null, source)
